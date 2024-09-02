b3040db7be69b3e5843413c336696931a9d79dbb
omnisonic <omnisonic@gmail.com>
2024-09-02 17:23:37 -0600

    add buy me coffee button

--- remove_background.py
+++ remove_background.py
@@ -41,3 +41,10 @@ if uploaded_file is not None:
 
     except Exception as e:
         st.error(f"Error processing image: {e}")
+
+button_code = '''
+<a href="https://www.buymeacoffee.com/jctecho"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=jctecho&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" /></a>
+'''
+
+st.markdown(button_code, unsafe_allow_html=True)
+
